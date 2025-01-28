a2a4ebb2a521d21254226971b2a06358db4d1316
Enzo Crance <enzo.crance@statnett.no>
2025-01-28 16:31:11 +0100

    test: test the identity resource (FLEX-429)

---
+++ test/api_client_tests/test_identity.py
@@ -0,0 +1,51 @@
+from security_token_service import (
+    SecurityTokenService,
+    TestEntity,
+)
+from flex.models import IdentityResponse, ErrorMessage
+from flex.api.identity import (
+    list_identity,
+    read_identity,
+)
+from typing import cast
+
+import pytest
+
+
+@pytest.fixture
+def sts():
+    yield SecurityTokenService()
+
+
+# RLS: ID-ENT001
+# RLS: ID-COM001
+def test_identity(sts):
+    client_ent = sts.get_client(TestEntity.TEST)
+
+    # generate clients before listing identities
+    # (the first generation of a client creates a new identity, so this loop
+    # iteration modifies the state of the very thing we test here)
+    clients = [sts.get_client(TestEntity.TEST, role) for role in sts.COMMON_ROLES]
+
+    ids = list_identity.sync(client=client_ent)
+    assert isinstance(ids, list)
+    nb_identities = len(ids)
+    assert nb_identities > 0
+
+    for client in clients:
+        # endpoint: GET /identity
+        ids = list_identity.sync(client=client)
+        assert isinstance(ids, list)
+        assert len(ids) == nb_identities
+
+        # endpoint: GET /identity/{id}
+        id = read_identity.sync(client=client, id=cast(int, ids[0].id))
+        assert isinstance(id, IdentityResponse)
+
+
+def test_event_anon(sts):
+    client = sts.get_client()
+
+    ids = list_identity.sync(client=client)
+    assert isinstance(ids, ErrorMessage)
+    assert ids.message.startswith("permission denied")
