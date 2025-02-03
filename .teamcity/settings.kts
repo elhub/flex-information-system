c881047d62bac1f2e35606800bd11c30ddca7c5e
Lise May Le <lise.le@statnett.no>
2025-02-03 15:20:03 +0100

    Try to change the scope of the customJob

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,4 +1,5 @@
 import jetbrains.buildServer.configs.kotlin.BuildType
+import jetbrains.buildServer.configs.kotlin.Project
 import jetbrains.buildServer.configs.kotlin.sequential
 import no.elhub.devxp.build.configuration.pipeline.Pipeline
 import no.elhub.devxp.build.configuration.pipeline.constants.ProjectType
@@ -9,14 +10,16 @@ import no.elhub.devxp.build.configuration.pipeline.utils.Stage
 
 import no.elhub.devxp.build.configuration.pipeline.ElhubProject
 import no.elhub.devxp.build.configuration.pipeline.ElhubProject.Companion.elhubProject
+import no.elhub.devxp.build.configuration.pipeline.constants.AgentScope
 import no.elhub.devxp.build.configuration.pipeline.constants.Group
 import no.elhub.devxp.build.configuration.pipeline.extensions.subProject
+import no.elhub.devxp.build.configuration.pipeline.jobs.customJob
 
-//elhubProject(Group.DEVXP, "flex-information-system") {
-//
-//    pipeline {
-//        sequential {
-//
+elhubProject(Group.DEVXP, "flex-information-system") {
+
+    pipeline {
+        sequential {
+
 //            val goSonarSettings: SonarScanSettings.Builder.() -> Unit = {
 //                sonarProjectSources = "backend"
 //                workingDir = "backend"
@@ -26,25 +29,37 @@ import no.elhub.devxp.build.configuration.pipeline.extensions.subProject
 //                sonarProjectSources = "frontend"
 //                workingDir = "frontend"
 //            }
-//
-////            goSonarScan(goSonarSettings)
-////            npmSonarScan(npmSonarSettings)
-//
-//            customJob(AgentScope.LinuxAgentContext) {
-//                id("GoSonarScan")
-//                name = "GoSonarScan"
-//                steps {
-////                    goSonarScan(goSonarSettings)
-//                    val settings = SonarScanSettings.Builder(projectContext, ProjectType.GO, goSonarSettings).build()
-//                    val sonarScan = SonarScan(settings)
-//                    val buildType = sonarScan.build(vcsSettings, teamcityProject.)
-//                }
-//            }
-//
-//
-//        }
-//    }
-//}
+
+//            goSonarScan(goSonarSettings)
+//            npmSonarScan(npmSonarSettings)
+
+
+            customJob(AgentScope.LinuxAgentContext) {
+                id("GoSonarScan")
+                name = "GoSonarScan"
+                steps {
+                    val goSonarSettings: SonarScanSettings.Builder.() -> Unit = {
+                        sonarProjectSources = "backend"
+                        workingDir = "backend"
+                    }
+
+                    val settings = SonarScanSettings.Builder(projectContext, ProjectType.GO, goSonarSettings).build()
+                    val sonarScan = SonarScan(settings)
+
+                    val buildType = sonarScan.build(vcsSettings, teamcityProject)
+
+                    stages = stages.plus(Stage.Single(buildType))
+
+                    // after adding the built job we can add the validation requirements
+                    validations = validations.plus(sonarScan.validations)
+
+                }
+            }
+
+
+        }
+    }
+}
 
 fun Pipeline.goSonarScan(block: SonarScanSettings.Builder.() -> Unit = {}): BuildType {
     val settings = SonarScanSettings.Builder(projectContext, ProjectType.GO, block).build()
@@ -71,40 +86,40 @@ internal fun Pipeline.addJob(job: Job): BuildType {
     return buildType
 }
 
-fun ElhubProject.customSubProject(projectName: String) {
-//    pipeline { this pipeline works, but only for backend
-//        sequential {
-//            val sonarScanSettings = SonarScanSettings.Builder(projectContext, ProjectType.GO, settings).build()
-//            val sonarScan = SonarScan(sonarScanSettings)
-//            addJob(sonarScan)
+//fun ElhubProject.customSubProject(projectName: String) {
+////    pipeline { this pipeline works, but only for backend
+////        sequential {
+////            val sonarScanSettings = SonarScanSettings.Builder(projectContext, ProjectType.GO, settings).build()
+////            val sonarScan = SonarScan(sonarScanSettings)
+////            addJob(sonarScan)
+////        }
+////    }
+//
+//    val subProject = subProject(projectName)
+//    subProject.sequential {
+//        pipeline {
+//            sequential {
+//                val goSonarSettings: SonarScanSettings.Builder.() -> Unit = {
+//                    sonarProjectSources = projectName
+//                    workingDir = projectName
+//                }
+//                val settings = SonarScanSettings.Builder(projectContext, ProjectType.GO, goSonarSettings).build()
+//                val sonarScanJob = SonarScan(settings)
+//                val buildType = sonarScanJob.build(vcsSettings, subProject)
+//                stages = stages.plus(Stage.Single(buildType))
+//
+//                // after adding the built job we can add the validation requirements
+//                validations = validations.plus(sonarScanJob.validations)
+//            }
 //        }
 //    }
-
-    val subProject = subProject(projectName)
-    subProject.sequential {
-        pipeline {
-            sequential {
-                val goSonarSettings: SonarScanSettings.Builder.() -> Unit = {
-                    sonarProjectSources = projectName
-                    workingDir = projectName
-                }
-                val settings = SonarScanSettings.Builder(projectContext, ProjectType.GO, goSonarSettings).build()
-                val sonarScanJob = SonarScan(settings)
-                val buildType = sonarScanJob.build(vcsSettings, subProject)
-                stages = stages.plus(Stage.Single(buildType))
-
-                // after adding the built job we can add the validation requirements
-                validations = validations.plus(sonarScanJob.validations)
-            }
-        }
-    }
-
-}
-
-elhubProject(Group.DEVXP, "flex-information-system") {
-    customSubProject("backend")
-//    customSubProject("frontend")
-}
+//
+//}
+//
+//elhubProject(Group.DEVXP, "flex-information-system") {
+//    customSubProject("backend")
+////    customSubProject("frontend")
+//}
 
 // For documentation on how to write these pipelines, see
 // https://docs.elhub.cloud/enabling-systems/devxp/devxp-build-configuration/user-guide.html
