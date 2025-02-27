f5f58a2262e62a878bdb2f7f464de1dfa6168359
Enzo Crance <enzo.crance@statnett.no>
2025-02-27 11:48:09 +0100

    feat(db): timeline function to check inclusion of range in timeline (FLEX-125)

--- db/flex/service_providing_group_membership_rls.sql
+++ db/flex/service_providing_group_membership_rls.sql
@@ -45,14 +45,18 @@ USING (EXISTS (
 ))
 WITH CHECK (
     EXISTS (
-        SELECT 1
+        SELECT
+            timeline_includes(
+                array_agg(cusp.valid_time_range),
+                service_providing_group_membership.valid_time_range -- noqa
+            )
         FROM controllable_unit_service_provider AS cusp
             INNER JOIN service_providing_group AS spg
                 ON cusp.service_provider_id = spg.service_provider_id
         WHERE spg.service_provider_id = current_party()
             AND cusp.controllable_unit_id = service_providing_group_membership.controllable_unit_id -- noqa
             AND spg.id = service_providing_group_membership.service_providing_group_id -- noqa
-            AND cusp.valid_time_range @> service_providing_group_membership.valid_time_range -- noqa
+            AND cusp.valid_time_range && service_providing_group_membership.valid_time_range -- noqa
     )
 );
 
