85ee65ef0701247b1e187c58f71e784d4349e98a
Enzo Crance <enzo.crance@statnett.no>
2025-03-10 17:46:53 +0100

    test: CU lookup test as SP (FLEX-478)

--- test/inspect/hurl.sh
+++ test/inspect/hurl.sh
@@ -12,6 +12,10 @@ PRIVATE_KEY_FILE="${SCRIPT_DIR}/../keys/.test.key.pem"
 HURL_grant_jwt=$("$SCRIPT_DIR/gen_jwt.py" "$PRIVATE_KEY_FILE")
 export HURL_grant_jwt
 
+# midnight date in +2 to +4 weeks window from now, for CUSP create operations
+HURL_window_ok_valid_from=$(date -d 'TZ="Europe/Oslo" +20 days 00:00' -u +'%Y-%m-%dT%TZ')
+export HURL_window_ok_valid_from
+
 hurl \
 	--variables-file "$1" \
 	--very-verbose \
