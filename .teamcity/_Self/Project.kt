449f948f41bcf6bf399b2dd91c7acde342a3c29f
Johannes Schaanning <johannes.schaanning@statnett.no>
2025-03-12 15:48:45 +0100

    Update build configuration and clean up teamcity structure

--- .teamcity/_Self/Project.kt
+++ .teamcity/_Self/Project.kt
@@ -2,18 +2,15 @@
 
 package _Self
 
-import no.elhub.devxp.build.configuration.pipeline.ElhubProject
+import jetbrains.buildServer.configs.kotlin.Project
 import no.elhub.devxp.build.configuration.pipeline.constants.Group
-import no.elhub.devxp.build.configuration.pipeline.context.ElhubProjectContext
-import jetbrains.buildServer.configs.kotlin.Project as TeamcityProject
+import no.elhub.devxp.build.configuration.pipeline.dsl.elhubProject
+
+typealias TeamcityProject = Project
 
 object Project : TeamcityProject({
-    val group = Group.FLEX
-    val name = "flex-information-system"
-    val projectContext = ElhubProjectContext(group, name)
-
-    ElhubProject(projectContext, this).apply {
-        subProject(GoProject.Project)
-        subProject(NpmProject.Project)
+    elhubProject(Group.FLEX, "flex-information-system", this) {
+        subProject(GoProject)
+        subProject(NpmProject)
     }
 })
