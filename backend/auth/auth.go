5c16921eeac02e05190e1a48c2371074b5b187b6
Enzo Crance <enzo.crance@statnett.no>
2025-03-13 13:34:25 +0100

    docs: rewrite is not only for PostgREST (FLEX-497)

--- backend/auth/auth.go
+++ backend/auth/auth.go
@@ -112,7 +112,8 @@ func (auth *API) TokenDecodingMiddleware(
 		} else { // no authorization header means we must use the cookie
 			tokenStr = sessionCookie.Value
 
-			// we set the header for cases where the request is sent to PostgREST
+			// we set the header for cases where the request is proxied or forwarded
+			// to another upstream service
 			req.Header.Set("Authorization", "Bearer "+tokenStr)
 		}
 
