8ef241a005b5cd9d963dfec9844318fa5c72953f
Enzo Crance <enzo.crance@statnett.no>
2025-04-08 15:58:23 +0200

    feat(ui): allow filtering of entities and parties (FLEX-568)

--- frontend/src/entity/EntityList.tsx
+++ frontend/src/entity/EntityList.tsx
@@ -1,14 +1,31 @@
-import { List, TextField } from "react-admin";
+import { List, SelectArrayInput, TextField } from "react-admin";
 import { Datagrid } from "../auth";
 
-export const EntityList = () => (
-  <List perPage={25} sort={{ field: "id", order: "DESC" }}>
-    <Datagrid>
-      <TextField source="id" />
-      <TextField source="name" />
-      <TextField source="type" />
-      <TextField source="business_id" label="Business ID" />
-      <TextField source="business_id_type" label="Business ID type" />
-    </Datagrid>
-  </List>
-);
+export const EntityList = () => {
+  const entityFilters = [
+    <SelectArrayInput
+      key="type"
+      label="Type"
+      source="type@in"
+      choices={["organisation", "person"]}
+      alwaysOn
+    />,
+  ];
+
+  return (
+    <List
+      perPage={25}
+      sort={{ field: "id", order: "DESC" }}
+      empty={false}
+      filters={entityFilters}
+    >
+      <Datagrid>
+        <TextField source="id" />
+        <TextField source="name" />
+        <TextField source="type" />
+        <TextField source="business_id" label="Business ID" />
+        <TextField source="business_id_type" label="Business ID type" />
+      </Datagrid>
+    </List>
+  );
+};
--- frontend/src/party/PartyList.tsx
+++ frontend/src/party/PartyList.tsx
@@ -1,20 +1,62 @@
-import { List, ReferenceField, TextField } from "react-admin";
-import { Datagrid } from "../auth";
+import { List, ReferenceField, SelectArrayInput, TextField } from "react-admin";
+import { AutocompleteReferenceInput, Datagrid } from "../auth";
 import { DateField } from "../datetime";
 
-export const PartyList = () => (
-  <List perPage={25} sort={{ field: "id", order: "DESC" }}>
-    <Datagrid>
-      <TextField source="id" label="ID" />
-      <TextField source="business_id" label="Business ID" />
-      <ReferenceField source="entity_id" reference="entity">
+export const PartyList = () => {
+  const partyFilters = [
+    <AutocompleteReferenceInput
+      key="entity_id"
+      source="entity_id"
+      reference="entity"
+      alwaysOn
+    />,
+    <SelectArrayInput
+      key="type"
+      label="Party type"
+      source="type@in"
+      choices={[
+        { id: "balance_responsible_party", name: "Balance Responsible Party" },
+        { id: "end_user", name: "End User" },
+        { id: "energy_supplier", name: "Energy Supplier" },
+        {
+          id: "flexibility_information_system_operator",
+          name: "Flexibility Information System Operator",
+        },
+        { id: "market_operator", name: "Market Operator" },
+        { id: "service_provider", name: "Service Provider" },
+        { id: "system_operator", name: "System Operator" },
+        { id: "third_party", name: "Third Party" },
+      ]}
+      alwaysOn
+    />,
+    <SelectArrayInput
+      key="status"
+      label="Status"
+      source="status@in"
+      choices={["new", "active", "inactive", "suspended", "terminated"]}
+      alwaysOn
+    />,
+  ];
+
+  return (
+    <List
+      perPage={25}
+      sort={{ field: "id", order: "DESC" }}
+      empty={false}
+      filters={partyFilters}
+    >
+      <Datagrid>
+        <TextField source="id" label="ID" />
+        <TextField source="business_id" label="Business ID" />
+        <ReferenceField source="entity_id" reference="entity">
+          <TextField source="name" />
+        </ReferenceField>
         <TextField source="name" />
-      </ReferenceField>
-      <TextField source="name" />
-      <TextField source="type" />
-      <TextField source="role" />
-      <TextField source="status" />
-      <DateField source="recorded_at" showTime />
-    </Datagrid>
-  </List>
-);
+        <TextField source="type" />
+        <TextField source="role" />
+        <TextField source="status" />
+        <DateField source="recorded_at" showTime />
+      </Datagrid>
+    </List>
+  );
+};
