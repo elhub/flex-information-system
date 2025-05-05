3384bd5b3b31b338ab2e22edd736db6fe4481b52
Enzo Crance <enzo.crance@statnett.no>
2025-05-05 16:12:19 +0200

    fix(test): stable AP-BRP test as SP (FLEX-490)

--- test/api_client_tests/test_accounting_point_balance_responsible_party.py
+++ test/api_client_tests/test_accounting_point_balance_responsible_party.py
@@ -2,7 +2,10 @@ from security_token_service import (
     SecurityTokenService,
     TestEntity,
 )
-from flex.models import AccountingPointBalanceResponsiblePartyResponse
+from flex.models import (
+    AccountingPointBalanceResponsiblePartyResponse,
+    AccountingPointBalanceResponsiblePartyEnergyDirection,
+)
 from flex.api.accounting_point_balance_responsible_party import (
     list_accounting_point_balance_responsible_party,
 )
@@ -58,25 +61,27 @@ def test_apbrp_sp(sts):
     # - then Common BRP until 2024-09-10
     # - then Test BRP until 2099-01-01 (far in the future)
 
-    # We check that:
-
-    # - 3 APs, each one having 3 AP-BRPs in production (of which 1 gets split
-    #   because of the blind month in August) and 1 in consumption (that also
-    #   gets split), so SP should see 3 x (3+1 + 1+1) = 18 records in total
     apbrps = list_accounting_point_balance_responsible_party.sync(
         client=client_sp,
     )
     assert isinstance(apbrps, list)
-    assert len(apbrps) == 18
+    assert len(apbrps) > 0
 
-    apbrps_ap1 = [
-        cast(AccountingPointBalanceResponsiblePartyResponse, apbrp)
-        for apbrp in apbrps
-        if apbrp.accounting_point_id == 1001
-    ]
+    apbrps_ap = sorted(
+        [
+            cast(AccountingPointBalanceResponsiblePartyResponse, apbrp)
+            for apbrp in apbrps
+            if apbrp.accounting_point_id == 1003
+            and apbrp.energy_direction
+            == AccountingPointBalanceResponsiblePartyEnergyDirection.PRODUCTION
+        ],
+        key=lambda apbrp: str(apbrp.valid_from),
+    )
+
+    # We check that:
 
     # - the first AP-BRP is cut before July (because SP has no CU before then)
-    apbrp0_start = datetime.fromisoformat(str(apbrps_ap1[0].valid_from)).astimezone(
+    apbrp0_start = datetime.fromisoformat(str(apbrps_ap[0].valid_from)).astimezone(
         tz=None
     )
     assert apbrp0_start.month == 7
@@ -84,17 +89,17 @@ def test_apbrp_sp(sts):
 
     # - the second AP-BRP is cut into 2 records, so that SP is blind in August
     assert (
-        apbrps_ap1[1].balance_responsible_party_id
-        == apbrps_ap1[2].balance_responsible_party_id
+        apbrps_ap[1].balance_responsible_party_id
+        == apbrps_ap[2].balance_responsible_party_id
     )
-    apbrp1_end = datetime.fromisoformat(str(apbrps_ap1[1].valid_to)).astimezone(tz=None)
+    apbrp1_end = datetime.fromisoformat(str(apbrps_ap[1].valid_to)).astimezone(tz=None)
     assert apbrp1_end.month == 8
     assert apbrp1_end.day == 1
-    apbrp2_start = datetime.fromisoformat(str(apbrps_ap1[2].valid_from)).astimezone(
+    apbrp2_start = datetime.fromisoformat(str(apbrps_ap[2].valid_from)).astimezone(
         tz=None
     )
     assert apbrp2_start.month == 9
     assert apbrp2_start.day == 1
 
     # - the end date of the third AP-BRP is not shown (too far in the future)
-    assert apbrps_ap1[3].valid_to is None
+    assert apbrps_ap[3].valid_to is None
