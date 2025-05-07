4b885ace18bc900b5e0966df9e2ecf23e72e5546
Kristoffer Lorentsen <kristoffer.lorentsen@statnett.no>
2025-05-07 13:11:09 +0200

    refactor: load with liquibase (FLEX-502)

--- db/flex/identity.sql
+++ db/flex/identity.sql
@@ -14,3 +14,30 @@ CREATE TABLE IF NOT EXISTS identity (
     CONSTRAINT uk_identity_eid UNIQUE (eid),
     CONSTRAINT uk_identity_entity_id_party_id UNIQUE (entity_id, party_id)
 );
+
+-- changeset flex:identity-external-id runOnChange:true endDelimiter:--
+CREATE OR REPLACE FUNCTION
+identity_external_id(p_entity_id bigint, p_party_id bigint) RETURNS uuid
+LANGUAGE plpgsql SECURITY DEFINER VOLATILE
+AS $$
+declare
+  lv_eid uuid;
+begin
+
+    select
+      eid into lv_eid
+    from flex.identity i
+    where i.entity_id is not distinct from p_entity_id
+    and i.party_id is not distinct from p_party_id;
+
+    -- identity does not yet exists, so we need to create it
+    if lv_eid is null then
+      insert into flex.identity (entity_id, party_id)
+      values (p_entity_id, p_party_id)
+      returning eid into lv_eid;
+    end if;
+
+    return lv_eid;
+
+end;
+$$;
