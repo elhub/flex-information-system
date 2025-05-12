fc1a132704cd1218c6e1302a95c4a20d6167224a
Enzo Crance <enzo.crance@statnett.no>
2025-05-12 15:14:57 +0200

    feat(ui): remove double API call and fix pagination (FLEX-576)

--- frontend/src/controllable_unit/balance_responsible_party/ControllableUnitBalanceResponsiblePartyList.tsx
+++ frontend/src/controllable_unit/balance_responsible_party/ControllableUnitBalanceResponsiblePartyList.tsx
@@ -4,48 +4,31 @@ import {
   ResourceContextProvider,
   SortPayload,
   TextField,
-  useGetList,
   usePermissions,
   useRecordContext,
 } from "react-admin";
 import { Datagrid } from "../../auth";
 import { DateField } from "../../datetime";
-import { CircularProgress } from "@mui/material";
 
 export const ControllableUnitBalanceResponsiblePartyList = () => {
   // accounting point id of the controllable unit whose BRPs we want to get
   const { accounting_point_id } = useRecordContext()!;
   const { permissions } = usePermissions();
 
-  // get accounting point technical ID of the controllable unit
-  const { data: apData, isPending } = useGetList("accounting_point", {
-    filter: { business_id: accounting_point_id },
-  });
-
-  // this allows querying the AP-BRP resource
   const sort: SortPayload = { field: "valid_from", order: "ASC" };
-  const { data, isLoading } = useGetList(
-    "accounting_point_balance_responsible_party",
-    {
-      filter: { accounting_point_id: apData ? apData[0].id : 0 },
-      sort,
-    },
-  );
-
-  if (isPending) {
-    return <CircularProgress size={25} thickness={2} />;
-  }
+  const filter = { accounting_point_id: accounting_point_id };
 
   return (
     permissions.includes("accounting_point_balance_responsible_party.read") && (
       <ResourceContextProvider value="accounting_point_balance_responsible_party">
-        <List title={false} perPage={10} exporter={false} empty={false}>
-          <Datagrid
-            bulkActionButtons={false}
-            data={data}
-            isLoading={isLoading}
-            sort={sort}
-          >
+        <List
+          title={false}
+          perPage={10}
+          exporter={false}
+          empty={false}
+          filter={filter}
+        >
+          <Datagrid bulkActionButtons={false} sort={sort}>
             <TextField source="id" label="ID" />
             <ReferenceField
               source="balance_responsible_party_id"
