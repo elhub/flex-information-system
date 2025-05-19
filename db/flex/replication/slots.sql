2f23159dbd9cc256a7ff8b56085ad7848bb2bb88
Enzo Crance <enzo.crance@statnett.no>
2025-05-19 08:54:19 +0200

    fix(db): create replication "if not exists" (FLEX-602) (#173)

--- db/flex/replication/slots.sql
+++ db/flex/replication/slots.sql
@@ -2,4 +2,8 @@
 -- Manually managed file
 
 -- changeset flex:api-create-event-logical-replication-slot endDelimiter:-- runAlways:true
-SELECT pg_create_logical_replication_slot('event_slot', 'wal2json');
+SELECT pg_create_logical_replication_slot('event_slot', 'wal2json')
+WHERE NOT EXISTS (
+        SELECT 1 FROM pg_replication_slots
+        WHERE slot_name = 'event_slot'
+    );
