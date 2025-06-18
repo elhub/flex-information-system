f9eab7f1d975d53640467538a7ec2853e4e5e519
Johannes Schaanning <johannes.schaanning@statnett.no>
2025-06-18 15:10:01 +0200

    feat: first draft for new pipeline

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,3 +1,69 @@
-import jetbrains.buildServer.configs.kotlin.project
+import jetbrains.buildServer.configs.kotlin.ArtifactRule
+import no.elhub.devxp.build.configuration.pipeline.constants.Group
+import no.elhub.devxp.build.configuration.pipeline.constants.KubeCluster
+import no.elhub.devxp.build.configuration.pipeline.dsl.elhubProject
+import no.elhub.devxp.build.configuration.pipeline.extensions.triggerOnVcsChange
+import no.elhub.devxp.build.configuration.pipeline.jobs.dockerBuild
+import no.elhub.devxp.build.configuration.pipeline.jobs.gitOps
+import no.elhub.devxp.build.configuration.pipeline.jobs.liquiBuild
+import no.elhub.devxp.build.configuration.pipeline.jobs.makeVerify
+import no.elhub.devxp.build.configuration.pipeline.jobs.npmVerify
 
-project(_Self.Project)
+elhubProject(Group.FLEX, "flex-information-system") {
+
+    val gitOpsRepo = "https://github.com/elhub/flex"
+    val imageRepo = "elhub/flex-information-system"
+
+    pipeline {
+        sequential {
+            makeVerify {
+                workingDir = "backend"
+                sonarScanSettings = {
+                    sonarProjectSources = "backend"
+                    workingDir = "backend"
+                }
+            }
+
+            dockerBuild {
+                dockerBuildNameSuffix = "backend"
+                dockerfileName = "path_to_dockerfile"
+            }.triggerOnVcsChange { triggerRules = "+:backend/**" }
+
+            liquiBuild {
+                registrySettings = {
+                    repository = imageRepo
+                }
+                changelogDirectory = "path_to_changelog_directory"
+            }.triggerOnVcsChange { triggerRules = "+:db/**" }
+
+            gitOps {
+                clusters = setOf(KubeCluster.TEST11) // TODO: Change this to relevant cluster
+                gitOpsRepository = gitOpsRepo
+            }.triggerOnVcsChange { triggerRules = "+:backend/**" }
+
+            sequential {
+                npmVerify {
+                    workingDir = "frontend"
+                    sonarScanSettings = {
+                        sonarProjectSources = "frontend/src"
+                        workingDir = "frontend"
+                        sonarProjectTests = null // no tests in frontend
+                    }
+                    buildArtifactRules = listOf(ArtifactRule.include("frontend/dist", "frontend/dist.zip"))
+                    outputArtifactRules = listOf(ArtifactRule.include("frontend/dist.zip!**", "frontend/dist"))
+                    analyzeDependencies = false // will be solved by TDX-587
+                }
+
+                dockerBuild {
+                    dockerBuildNameSuffix = "frontend"
+                    dockerfileName = "path_to_dockerfile"
+                }.triggerOnVcsChange { triggerRules = "+:frontend/**" }
+
+                gitOps {
+                    clusters = setOf(KubeCluster.TEST13) // TODO: Change this to relevant cluster
+                    gitOpsRepository = gitOpsRepo
+                }.triggerOnVcsChange { triggerRules = "+:frontend/**" }
+            }
+        }
+    }
+}
