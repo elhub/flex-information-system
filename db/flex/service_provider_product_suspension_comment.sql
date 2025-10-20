fb461a6e540ca13c51fe0bfc4307d0831e1b9f15
Enzo Crance <enzo.crance@statnett.no>
2025-10-20 12:31:48 +0200

    feat: generate tables for comment resources (FLEX-792)

--- db/flex/service_provider_product_suspension_comment.sql
+++ db/flex/service_provider_product_suspension_comment.sql
@@ -1,8 +1,7 @@
 --liquibase formatted sql
--- Manually managed file
+-- GENERATED CODE -- DO NOT EDIT (scripts/openapi_to_db.py)
 
--- changeset flex:service-provider-product-suspension-comment-create runOnChange:false endDelimiter:--
--- validCheckSum: 9:5e2a329a77b6d2feb6c1a208c0551e16
+-- changeset flex:service-provider-product-suspension-comment-create runOnChange:true endDelimiter:--
 CREATE TABLE IF NOT EXISTS service_provider_product_suspension_comment (
     id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     service_provider_product_suspension_id bigint NOT NULL,
@@ -24,14 +23,17 @@ CREATE TABLE IF NOT EXISTS service_provider_product_suspension_comment (
             'any_involved_party'
         )
     ),
-    CONSTRAINT service_provider_product_suspension_comment_sppa_fkey
+    CONSTRAINT service_provider_product_suspension_comment_spps_fkey
     FOREIGN KEY (service_provider_product_suspension_id)
     REFERENCES service_provider_product_suspension (id)
     ON DELETE CASCADE
 );
 
 -- changeset flex:service-provider-product-suspension-comment-capture-event runOnChange:true endDelimiter:--
-CREATE OR REPLACE TRIGGER service_provider_product_suspension_comment_event
-AFTER INSERT OR UPDATE ON service_provider_product_suspension_comment
+CREATE OR REPLACE TRIGGER
+service_provider_product_suspension_comment_event
+AFTER INSERT OR UPDATE
+ON service_provider_product_suspension_comment
 FOR EACH ROW
-EXECUTE FUNCTION capture_event('service_provider_product_suspension_comment');
+EXECUTE FUNCTION
+capture_event('service_provider_product_suspension_comment');
