711be004389c7fb19a4234f29c757af511eeead7
Enzo Crance <enzo.crance@statnett.no>
2025-11-03 16:34:01 +0100

    feat: SPG product suspension notices (FLEX-826)

--- db/flex/notice.sql
+++ db/flex/notice.sql
@@ -355,4 +355,68 @@ CREATE VIEW notice AS (
     FROM flex.service_providing_group_grid_suspension AS spggs
     WHERE lower(spggs.record_time_range)
         < current_timestamp - interval '2 weeks'
+
+    -- suspension on product type no longer qualified
+    UNION ALL
+    (
+        WITH
+            qualified_product_types AS (
+                SELECT
+                    spgpa.service_providing_group_id,
+                    spgpa.procuring_system_operator_id,
+                    array_agg(DISTINCT spgpa.product_type_id)
+                        AS product_type_ids -- noqa
+                FROM (
+                    SELECT
+                        spgpa.service_providing_group_id,
+                        spgpa.procuring_system_operator_id,
+                        unnest(spgpa.product_type_ids) AS product_type_id
+                    FROM
+                        flex.service_providing_group_product_application
+                            AS spgpa -- noqa
+                    WHERE (
+                        spgpa.prequalified_at IS NOT null
+                        OR spgpa.verified_at IS NOT null
+                    )
+                ) AS spgpa
+                GROUP BY
+                    spgpa.service_providing_group_id,
+                    spgpa.procuring_system_operator_id
+            )
+
+        SELECT
+            spgps.procuring_system_operator_id AS party_id,
+            'no.elhub.flex.service_providing_group_product_suspension.product_type.not_qualified' AS type, -- noqa
+            '/service_providing_group_product_suspension/'
+            || spgps.id AS source,
+            jsonb_build_object(
+                'product_type_ids', (
+                    SELECT array(
+                        SELECT unnest(spgps.product_type_ids)
+                        EXCEPT
+                        SELECT unnest(coalesce(qpts.product_type_ids, '{}'))
+                    )
+                )
+            ) AS data -- noqa
+        FROM flex.service_providing_group_product_suspension AS spgps
+            LEFT JOIN qualified_product_types AS qpts
+                ON spgps.service_providing_group_id
+                    = qpts.service_providing_group_id
+                    AND spgps.procuring_system_operator_id
+                    = qpts.procuring_system_operator_id
+        WHERE NOT spgps.product_type_ids
+            <@ coalesce(qpts.product_type_ids, '{}')
+    )
+
+    -- inactive suspension
+    UNION ALL
+    SELECT
+        spgps.procuring_system_operator_id AS party_id,
+        'no.elhub.flex.service_providing_group_product_suspension.lingering'
+            AS type, -- noqa
+        '/service_providing_group_product_suspension/' || spgps.id AS source,
+        null::jsonb AS data -- noqa
+    FROM flex.service_providing_group_product_suspension AS spgps
+    WHERE lower(spgps.record_time_range)
+        < current_timestamp - interval '2 weeks'
 );
