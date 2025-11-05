07d83e23323ea227545b77ae810e8238f5e2af08
sigrid.oconnell <sigrid.oconnell@statnett.no>
2025-11-05 10:56:26 +0100

    fix: updated so testes (FLEX-821)

--- test/api_client_tests/test_cu_suspension.py
+++ test/api_client_tests/test_cu_suspension.py
@@ -9,6 +9,7 @@ from flex.models import (
     ControllableUnitResponse,
     ControllableUnitServiceProviderCreateRequest,
     ControllableUnitServiceProviderResponse,
+    ServiceProvidingGroupResponse,
     ErrorMessage,
     EmptyObject,
 )
@@ -18,6 +19,11 @@ from flex.models import (
     ControllableUnitSuspensionReason,
     ControllableUnitSuspensionUpdateRequest,
     ControllableUnitSuspensionHistoryResponse,
+    ServiceProvidingGroupCreateRequest,
+    ServiceProvidingGroupProductApplicationCreateRequest,
+    ServiceProvidingGroupProductApplicationResponse,
+    ServiceProvidingGroupMembershipCreateRequest,
+    ServiceProvidingGroupMembershipResponse,
 )
 from flex.api.controllable_unit_suspension import (
     list_controllable_unit_suspension,
@@ -27,6 +33,39 @@ from flex.api.controllable_unit_suspension import (
     delete_controllable_unit_suspension,
     list_controllable_unit_suspension_history,
     read_controllable_unit_suspension_history,
+)
+from flex.api.service_providing_group_membership import (
+    create_service_providing_group_membership,
+)
+from flex.api.service_providing_group import (
+    read_service_providing_group,
+    update_service_providing_group,
+)
+from flex.models import (
+    ServiceProvidingGroupUpdateRequest,
+    ServiceProvidingGroupStatus,
+    SystemOperatorProductTypeCreateRequest,
+    SystemOperatorProductTypeResponse,
+    ServiceProviderProductApplicationCreateRequest,
+    ServiceProviderProductApplicationResponse,
+    ServiceProviderProductApplicationUpdateRequest,
+    ServiceProviderProductApplicationStatus,
+)
+
+
+from flex.api.service_providing_group_product_application import (
+    create_service_providing_group_product_application,
+)
+from flex.api.system_operator_product_type import (
+    create_system_operator_product_type,
+)
+from flex.api.service_provider_product_application import (
+    create_service_provider_product_application,
+    update_service_provider_product_application,
+)
+
+from flex.api.service_providing_group import (
+    create_service_providing_group,
 )
 
 from flex.api.controllable_unit import (
@@ -107,7 +146,7 @@ def check_history(client, cus_id):
 def test_cus_fiso(data):
     (sts, cu_id, (_, so_id), _) = data
 
-    client_fiso = sts.get_client(TestEntity.TEST, "FISO")
+    client_fiso = sts.fresh_client(TestEntity.TEST, "FISO")
 
     # endpoint: POST /service_providing_group_grid_suspension
     cus = create_controllable_unit_suspension.sync(
@@ -155,7 +194,7 @@ def test_cus_fiso(data):
 
 
 def test_cus_so(data):
-    (_, cu_id, (client_so, so_id), _) = data
+    (sts, cu_id, (client_so, so_id), (client_sp, sp_id)) = data
 
     # RLS: CUS-SO001
     # SO can create a suspension for a CU that impacts them
@@ -193,15 +232,145 @@ def test_cus_so(data):
     assert not isinstance(u, ErrorMessage)
 
     # RLS: CUS-SO003
-    r = read_controllable_unit_suspension.sync(
-        client=client_so,
-        id=cast(int, s.id),
+
+    client_fiso = sts.fresh_client(TestEntity.TEST, "FISO")
+    client_so2 = sts.fresh_client(TestEntity.COMMON, "SO")
+    so2_id = sts.get_userinfo(client_so2)["party_id"]
+
+    client_sp = sts.fresh_client(TestEntity.TEST, "SP")
+    sp_id = sts.get_userinfo(client_sp)["party_id"]
+
+    # create CU and SPG, link CU to SP in charge of the SPG,
+    # add CU to SPG, mark SO as impacted for SPG
+
+    cu = create_controllable_unit.sync(
+        client=client_sp,
+        body=ControllableUnitCreateRequest(
+            name="TEST-CU-18",
+            accounting_point_id=1002,
+            regulation_direction=ControllableUnitRegulationDirection.UP,
+            maximum_available_capacity=2,
+        ),
     )
-    assert isinstance(r, ControllableUnitSuspensionResponse)
-    assert r.impacted_system_operator_id == so_id
+    assert isinstance(cu, ControllableUnitResponse)
+
+    spg = create_service_providing_group.sync(
+        client=client_fiso,
+        body=ServiceProvidingGroupCreateRequest(
+            service_provider_id=sp_id,
+            name="New group",
+        ),
+    )
+    assert isinstance(spg, ServiceProvidingGroupResponse)
+
+    cus = create_controllable_unit_suspension.sync(
+        client=client_so,
+        body=ControllableUnitSuspensionCreateRequest(
+            impacted_system_operator_id=so_id,
+            controllable_unit_id=cast(int, cu.id),
+            reason=ControllableUnitSuspensionReason.OTHER,
+        ),
+    )
+    assert isinstance(cus, ControllableUnitSuspensionResponse)
+
+    # SO cannot read the SPG yet (cf SPG RLS below)
+    err = read_service_providing_group.sync(
+        client=client_so2,
+        id=cast(int, spg.id),
+    )
+    assert isinstance(err, ErrorMessage)
+
+    spgm = create_service_providing_group_membership.sync(
+        client=client_fiso,
+        body=ServiceProvidingGroupMembershipCreateRequest(
+            controllable_unit_id=cast(int, cu.id),
+            service_providing_group_id=cast(int, spg.id),
+            valid_from="2024-01-01T00:00:00+1",
+        ),
+    )
+    assert isinstance(spgm, ServiceProvidingGroupMembershipResponse)
+
+    u = update_service_providing_group.sync(
+        client=client_fiso,
+        id=cast(int, spg.id),
+        body=ServiceProvidingGroupUpdateRequest(
+            status=ServiceProvidingGroupStatus.ACTIVE,
+        ),
+    )
+    assert not isinstance(u, ErrorMessage)
+
+    pt_ids = [5, 7]
+
+    sopt5 = create_system_operator_product_type.sync(
+        client=client_so2,
+        body=SystemOperatorProductTypeCreateRequest(
+            system_operator_id=so2_id,
+            product_type_id=5,
+        ),
+    )
+    assert isinstance(sopt5, SystemOperatorProductTypeResponse)
+
+    sopt7 = create_system_operator_product_type.sync(
+        client=client_so2,
+        body=SystemOperatorProductTypeCreateRequest(
+            system_operator_id=so2_id,
+            product_type_id=7,
+        ),
+    )
+    assert isinstance(sopt7, SystemOperatorProductTypeResponse)
+
+    sppa = create_service_provider_product_application.sync(
+        client=client_sp,
+        body=ServiceProviderProductApplicationCreateRequest(
+            service_provider_id=sp_id,
+            system_operator_id=so2_id,
+            product_type_ids=pt_ids,
+        ),
+    )
+
+    # gir feilen message='product type automatic_fast_frequency is not active for system operator'
+    assert isinstance(sppa, ServiceProviderProductApplicationResponse)
+
+    u = update_service_provider_product_application.sync(
+        client=client_so2,
+        id=cast(int, sppa.id),
+        body=ServiceProviderProductApplicationUpdateRequest(
+            status=ServiceProviderProductApplicationStatus.QUALIFIED,
+            qualified_at="2024-01-01T00:00:00+1",
+        ),
+    )
+
+    assert not isinstance(u, ErrorMessage)
+
+    spgpa = create_service_providing_group_product_application.sync(
+        client=client_sp,
+        body=ServiceProvidingGroupProductApplicationCreateRequest(
+            service_providing_group_id=cast(int, spg.id),
+            procuring_system_operator_id=so2_id,
+            product_type_ids=pt_ids,
+        ),
+    )
+    assert isinstance(spgpa, ServiceProvidingGroupProductApplicationResponse)
+
+    u = update_service_provider_product_application.sync(
+        client=client_so2,
+        id=cast(int, sppa.id),
+        body=ServiceProviderProductApplicationUpdateRequest(
+            status=ServiceProviderProductApplicationStatus.QUALIFIED,
+            qualified_at="2024-01-01T00:00:00+1",
+        ),
+    )
+    assert not isinstance(u, ErrorMessage)
+
+    # SO can read the SPG now
+    s = read_controllable_unit_suspension.sync(
+        client=client_so2,
+        id=cast(int, cus.id),
+    )
+    assert isinstance(s, ControllableUnitSuspensionResponse)
 
     # RLS: CUS-SO004
-    check_history(client_so, s.id)
+    check_history(client_so2, s.id)
 
 
 def test_cus_sp(data):
@@ -213,7 +382,7 @@ def test_cus_sp(data):
     # endpoint: GET /controllable_unit_suspension_suspension
     cus = list_controllable_unit_suspension.sync(client=client_sp)
 
-    sp2_client = sts.get_client(TestEntity.COMMON, "SP")
+    sp2_client = sts.fresh_client(TestEntity.COMMON, "SP")
 
     assert isinstance(cus, list)
 
