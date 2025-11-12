3b146ebf67cdf9c94ad53658276dfb1bbc3c57f7
Enzo Crance <enzo.crance@statnett.no>
2025-11-12 15:48:04 +0100

    refactor: APEU -> CUEU (FLEX-848)

--- backend/event/schemas/notification.sql
+++ backend/event/schemas/notification.sql
@@ -2,13 +2,10 @@
 
 CREATE SCHEMA notification;
 
-CREATE TABLE notification.accounting_point_end_user (
-    id bigint NOT NULL,
-    accounting_point_id bigint NOT NULL,
+CREATE TABLE notification.controllable_unit_end_user (
+    controllable_unit_id bigint NOT NULL,
     end_user_id bigint NOT NULL,
-    valid_time_range tstzrange NOT NULL,
-    record_time_range tstzrange NOT NULL,
-    recorded_by bigint NOT NULL
+    valid_time_range tstzrange NOT NULL
 );
 
 CREATE TABLE notification.controllable_unit_system_operator (
