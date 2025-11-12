3d5cedf98dc63f796e0513b5cca6bb58ce2b97de
Enzo Crance <enzo.crance@statnett.no>
2025-11-12 14:27:29 +0100

    refactor: swap 2 tables for readability (FLEX-848)

--- backend/event/schemas/notification.sql
+++ backend/event/schemas/notification.sql
@@ -2,12 +2,6 @@
 
 CREATE SCHEMA notification;
 
-CREATE TABLE notification.controllable_unit_system_operator (
-    controllable_unit_id bigint NOT NULL,
-    system_operator_id bigint NOT NULL,
-    valid_time_range tstzrange NOT NULL
-);
-
 CREATE TABLE notification.accounting_point_end_user (
     id bigint NOT NULL,
     accounting_point_id bigint NOT NULL,
@@ -15,4 +9,10 @@ CREATE TABLE notification.accounting_point_end_user (
     valid_time_range tstzrange NOT NULL,
     record_time_range tstzrange NOT NULL,
     recorded_by bigint NOT NULL
+);
+
+CREATE TABLE notification.controllable_unit_system_operator (
+    controllable_unit_id bigint NOT NULL,
+    system_operator_id bigint NOT NULL,
+    valid_time_range tstzrange NOT NULL
 );
