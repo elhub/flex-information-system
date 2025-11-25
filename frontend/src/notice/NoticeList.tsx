a416a1b13be8a29cd6a9b8ebfd7b1ef775f308a2
August Solvang <august.solvang@statnett.no>
2025-11-25 09:13:37 +0100

    feat: add notice types and filter on party in notice list (FLEX-860)

--- frontend/src/notice/NoticeList.tsx
+++ frontend/src/notice/NoticeList.tsx
@@ -1,7 +1,18 @@
-import { List, ReferenceField, TextField, useRecordContext } from "react-admin";
+import {
+  AutocompleteInput,
+  List,
+  ReferenceField,
+  SelectInput,
+  TextField,
+  useGetIdentity,
+  useGetList,
+  useList,
+  useRecordContext,
+} from "react-admin";
 import { Datagrid } from "../auth";
 import { ResourceButton } from "../components/ResourceButton";
 import { NoticeShow } from "./NoticeShow";
+import noticeTypes from "./noticeTypes";
 
 const NoticeResourceButton = () => {
   const noticeRecord = useRecordContext()!;
@@ -10,17 +21,50 @@ const NoticeResourceButton = () => {
   ) : null;
 };
 
-export const NoticeList = () => (
+export const NoticeList = () => {
+  const { data: identity, isPending: isPendingIdentity } = useGetIdentity();
+  const { data: parties } = useGetList("party");
+
+  if (isPendingIdentity) return <p>Loading...</p>;
+
+  const filters = [
+    <AutocompleteInput
+      choices={parties?.map((p) => ({ id: p.id, name: p.name })) || []}
+      source="party_id"
+      label="Party"
+      defaultValue={identity?.party_id}
+    ></AutocompleteInput>,
+    <AutocompleteInput
+      source="type"
+      slotProps={{
+        popper: {
+          style: {
+            width: "fit-content",
+          },
+        },
+      }}
+      choices={noticeTypes.map((nt) => ({ id: nt.id, name: nt.label }))}
+      label="Notice type"
+    ></AutocompleteInput>,
+  ];
+
   // a defined sort parameter is required there because notice has no ID field
   // cf https://github.com/marmelab/react-admin/blob/27dccfb8519de551ef7e236355860aacef36ef56/packages/ra-core/src/controller/list/useListController.ts#L451-L454
-  <List perPage={25} sort={{ field: "type", order: "ASC" }} empty={false}>
-    <Datagrid expand={NoticeShow} expandSingle={true}>
-      <ReferenceField source="party_id" reference="party" sortable={false}>
-        <TextField source="name" />
-      </ReferenceField>
-      <TextField source="type" />
-      <TextField source="source" />
-      <NoticeResourceButton />
-    </Datagrid>
-  </List>
-);
+  return (
+    <List
+      perPage={25}
+      filters={filters}
+      sort={{ field: "source", order: "DESC" }}
+      empty={false}
+    >
+      <Datagrid expand={NoticeShow} expandSingle={true}>
+        <ReferenceField source="party_id" reference="party" sortable={false}>
+          <TextField source="name" />
+        </ReferenceField>
+        <TextField source="type" />
+        <TextField source="source" />
+        <NoticeResourceButton />
+      </Datagrid>
+    </List>
+  );
+};
---
+++ frontend/src/notice/noticeTypes.ts
@@ -0,0 +1,85 @@
+const noticeTypes = [
+  {
+    id: "no.elhub.flex.controllable_unit.grid_node_id.missing",
+    label: "controllable_unit.grid_node_id.missing",
+  },
+  {
+    id: "no.elhub.flex.controllable_unit.grid_validation_status.incomplete_information",
+    label: "controllable_unit.grid_validation_status.incomplete_information",
+  },
+  {
+    id: "no.elhub.flex.controllable_unit.grid_validation_status.pending",
+    label: "controllable_unit.grid_validation_status.pending",
+  },
+  {
+    id: "no.elhub.flex.controllable_unit_service_provider.valid_time.outside_contract",
+    label: "controllable_unit_service_provider.valid_time.outside_contract",
+  },
+  {
+    id: "no.elhub.flex.controllable_unit_suspension.not_active",
+    label: "controllable_unit_suspension.not_active",
+  },
+  {
+    id: "no.elhub.flex.controllable_unit_suspension.lingering",
+    label: "controllable_unit_suspension.lingering",
+  },
+  {
+    id: "no.elhub.flex.party.missing",
+    label: "party.missing",
+  },
+  {
+    id: "no.elhub.flex.party.outdated",
+    label: "party.outdated",
+  },
+  {
+    id: "no.elhub.flex.party.residual",
+    label: "party.residual",
+  },
+  {
+    id: "no.elhub.flex.service_provider_product_application.status.requested",
+    label: "service_provider_product_application.status.requested",
+  },
+  {
+    id: "no.elhub.flex.service_providing_group.balance_responsible_party.multiple",
+    label: "service_providing_group.balance_responsible_party.multiple",
+  },
+  {
+    id: "no.elhub.flex.service_providing_group_grid_prequalification.status.requested",
+    label: "service_providing_group_grid_prequalification.status.requested",
+  },
+  {
+    id: "no.elhub.flex.service_providing_group_membership.valid_time.outside_contract",
+    label: "service_providing_group_membership.valid_time.outside_contract",
+  },
+  {
+    id: "no.elhub.flex.service_providing_group_product_application.status.requested",
+    label: "service_providing_group_product_application.status.requested",
+  },
+  {
+    id: "no.elhub.flex.service_provider_product_suspension.product_id.not_qualified",
+    label: "service_provider_product_suspension.product_id.not_qualified",
+  },
+  {
+    id: "no.elhub.flex.service_provider_product_suspension.lingering",
+    label: "service_provider_product_suspension.lingering",
+  },
+  {
+    id: "no.elhub.flex.service_providing_group_grid_suspension.not_grid_prequalified",
+    label: "service_providing_group_grid_suspension.not_grid_prequalified",
+  },
+  {
+    id: "no.elhub.flex.service_providing_group_grid_suspension.lingering",
+    label: "service_providing_group_grid_suspension.lingering",
+  },
+  {
+    id: "no.elhub.flex.service_providing_group_product_suspension.product_id.not_qualified",
+    label:
+      "service_providing_group_product_suspension.product_id.not_qualified",
+  },
+  {
+    id: "no.elhub.flex.service_providing_group_product_suspension.lingering",
+    label: "service_providing_group_product_suspension.lingering",
+  },
+];
+
+export default noticeTypes;
