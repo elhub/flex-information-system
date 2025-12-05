6f2fd1e9279a4813f089948941dfe190ade5f2b5
Kristoffer Lorentsen <kristoffer.lorentsen@statnett.no>
2025-12-05 10:44:57 +0100

    feat: add spgm bidding zone consistency validation (FLEX-900)

--- db/flex/service_providing_group_membership.sql
+++ db/flex/service_providing_group_membership.sql
@@ -76,6 +76,52 @@ FOR EACH ROW EXECUTE PROCEDURE consistency_on_spgm_valid_time_increase();
 DROP TRIGGER IF EXISTS service_providing_group_membership_upsert_consistency
 ON service_providing_group_membership;
 
+-- changeset flex:service-providing-group-membership-bidding-zone-consistency runOnChange:true endDelimiter:--
+-- SPGM-VAL002: The bidding zone of the SPGM must be consistent with the bidding zone of the CU.
+CREATE OR REPLACE FUNCTION
+service_providing_group_membership_bidding_zone_consistency()
+RETURNS trigger
+LANGUAGE plpgsql
+SECURITY DEFINER
+AS $$
+DECLARE
+  -- the new SPGM is covered by the CU-BZ valid timeline
+  lv_covered boolean;
+  lv_spg_bidding_zone text;
+BEGIN
+
+    select bidding_zone into lv_spg_bidding_zone
+    from flex.service_providing_group spg
+    where spg.id = NEW.service_providing_group_id;
+
+    select
+        COALESCE(range_agg(cubz.valid_time_range) @> NEW.valid_time_range, false)
+    into lv_covered
+    from flex.controllable_unit_bidding_zone as cubz
+    where cubz.controllable_unit_id = NEW.controllable_unit_id
+    and cubz.bidding_zone = lv_spg_bidding_zone
+    and cubz.valid_time_range && NEW.valid_time_range;
+
+    IF NOT lv_covered THEN
+      RAISE 'The CU is not in the SPGs bidding zone % from % to %',
+        lv_spg_bidding_zone,
+        timeline.timestamptz_to_text(lower(NEW.valid_time_range)),
+        timeline.timestamptz_to_text(upper(NEW.valid_time_range));
+    END IF;
+
+    RETURN NEW;
+END;
+$$;
+
+-- changeset flex:service-providing-group-membership-bidding-zone-consistency-trigger runOnChange:true endDelimiter:--
+CREATE OR REPLACE TRIGGER
+service_providing_group_membership_bidding_zone_consistency
+BEFORE INSERT OR UPDATE ON
+service_providing_group_membership
+FOR EACH ROW EXECUTE PROCEDURE
+service_providing_group_membership_bidding_zone_consistency();
+
+
 -- changeset flex:service-providing-group-insert-grip-prequalificaton-function runOnChange:true endDelimiter:--
 CREATE OR REPLACE FUNCTION
 spgm_insert_grid_prequalification()
