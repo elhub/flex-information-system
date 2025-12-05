9305ae0c4930f1548d76645da3582d4dc1b32b04
August Solvang <august.solvang@statnett.no>
2025-12-05 08:35:08 +0100

    feat: use generated zod to validate form

--- frontend/src/controllable_unit/lookup/ControllableUnitLookupInput.tsx
+++ frontend/src/controllable_unit/lookup/ControllableUnitLookupInput.tsx
@@ -11,43 +11,12 @@ import TravelExploreIcon from "@mui/icons-material/TravelExplore";
 import UndoIcon from "@mui/icons-material/Undo";
 import { useFormContext } from "react-hook-form";
 import { InputStack } from "../../auth";
-import { useEffect } from "react";
 import { callControllableUnitLookup } from "../../generated-client";
+import { zControllableUnitLookupRequest } from "../../generated-client/zod.gen";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 const Toolbar = () => {
   const navigate = useNavigate();
-  const { getValues } = useFormContext();
-  const notify = useNotify();
-
-  // launch lookup request
-  const lookup = async () => {
-    let body: any = {};
-    const values = getValues();
-
-    // do not put undefined/null into the request body
-    for (const key in values) {
-      if (values[key]) {
-        body[key] = values[key];
-      }
-    }
-    const response = await callControllableUnitLookup({
-      body: {
-        end_user: body.end_user,
-        accounting_point: body.accounting_point,
-        controllable_unit: body.controllable_unit,
-      },
-    });
-
-    if (response.error) {
-      // error, just notify the user like in the other pages
-      notify(response.error.message, { type: "error" });
-      return;
-    }
-    // navigate to the dedicated show page for the result
-    return navigate("/controllable_unit/lookup/result", {
-      state: { result: response.data },
-    });
-  };
 
   return (
     <RAToolbar>
@@ -55,7 +24,7 @@ const Toolbar = () => {
         color="primary"
         variant="contained"
         startIcon={<TravelExploreIcon />}
-        onClick={lookup}
+        type="submit"
       >
         Lookup
       </Button>
@@ -72,47 +41,70 @@ const Toolbar = () => {
   );
 };
 
+const ControllableUnitLookupForm = () => {
+  const { watch } = useFormContext();
+  const controllableUnit = watch("controllable_unit");
+  const accountingPoint = watch("accounting_point");
+
+  const accountingPointDisabled =
+    controllableUnit && controllableUnit.length > 0;
+  const controllableUnitDisabled =
+    accountingPoint && accountingPoint.length > 0;
+
+  return (
+    <InputStack direction="row" flexWrap="wrap">
+      <TextInput source="end_user" validate={required()} />
+      <TextInput source="accounting_point" disabled={accountingPointDisabled} />
+      <TextInput
+        source="controllable_unit"
+        disabled={controllableUnitDisabled}
+      />
+    </InputStack>
+  );
+};
+
 // page to enter data required for controllable unit lookup
 export const ControllableUnitLookupInput = () => {
   const { state } = useLocation();
+  const defaultControllableUnit = state?.controllable_unit as
+    | string
+    | undefined;
 
-  const ControllableUnitLookupForm = () => {
-    const { getValues, setValue } = useFormContext();
+  const navigate = useNavigate();
+  const notify = useNotify();
 
-    useEffect(() => {
-      if (state?.controllable_unit) {
-        setValue("controllable_unit", state?.controllable_unit);
-      }
+  // launch lookup request
+  const lookup = async (data: unknown) => {
+    const lookupRequest = zControllableUnitLookupRequest.safeParse(data);
+
+    if (!lookupRequest.success) {
+      notify("Invalid input data", { type: "error" });
+      return;
+    }
+
+    const response = await callControllableUnitLookup({
+      body: lookupRequest.data,
     });
 
-    const values = getValues();
-    const accountingPointDefined =
-      values.accounting_point && values.accounting_point.length > 0;
-    const controllableUnitDefined =
-      values.controllable_unit && values.controllable_unit.length > 0;
-
-    return (
-      <InputStack
-        direction="row"
-        flexWrap="wrap"
-        allowAll
-        resource="controllable_unit_lookup_request"
-      >
-        <TextInput source="end_user" validate={required()} />
-        <TextInput
-          source="accounting_point"
-          disabled={controllableUnitDefined}
-        />
-        <TextInput
-          source="controllable_unit"
-          disabled={accountingPointDefined}
-        />
-      </InputStack>
-    );
+    if (response.error) {
+      // error, just notify the user like in the other pages
+      notify(response.error.message, { type: "error" });
+      return;
+    }
+    // navigate to the dedicated show page for the result
+    return navigate("/controllable_unit/lookup/result", {
+      state: { result: response.data },
+    });
   };
 
   return (
-    <SimpleForm maxWidth={1280} toolbar={<Toolbar />}>
+    <SimpleForm
+      maxWidth={1280}
+      onSubmit={lookup}
+      defaultValues={{ controllable_unit: defaultControllableUnit }}
+      resolver={zodResolver(zControllableUnitLookupRequest.partial())}
+      toolbar={<Toolbar />}
+    >
       <Stack direction="column" spacing={1}>
         <Typography variant="h6" gutterBottom>
           Lookup a controllable unit
