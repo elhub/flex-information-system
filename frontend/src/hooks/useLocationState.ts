c894f8aaa8e069ca7305d6ca1f4c6af120581a91
August Solvang <august.solvang@statnett.no>
2025-12-05 08:49:16 +0100

    chore: remove unused hook

--- frontend/src/hooks/useLocationState.ts
+++
@@ -1,8 +0,0 @@
-import { useLocation } from "react-router-dom";
-
-const useLocationState = <T>() => {
-  const { state } = useLocation();
-  return state as T;
-};
-
-export default useLocationState;
