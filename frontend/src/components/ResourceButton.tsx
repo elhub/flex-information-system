36a859d56a7f2d9cdb440e28b39d4583ed34deac
Enzo Crance <enzo.crance@statnett.no>
2025-12-11 16:55:39 +0100

    fix(ui): resource button checks if resource exists before redirecting (FLEX-911)

--- frontend/src/components/ResourceButton.tsx
+++ frontend/src/components/ResourceButton.tsx
@@ -1,18 +1,35 @@
-import { Button, ButtonProps } from "react-admin";
-import { Link } from "react-router-dom";
+import { Button, ButtonProps, useDataProvider, useNotify } from "react-admin";
+import { useNavigate } from "react-router-dom";
 import DataObjectIcon from "@mui/icons-material/DataObject";
 
 // button used in event-related resources to navigate to the concerned resource
 export const ResourceButton = (props: ButtonProps & { source: string }) => {
   const { source, ...rest } = props;
 
+  const dataProvider = useDataProvider();
+  const navigate = useNavigate();
+  const notify = useNotify();
+
   const resource = source.split("/")[1];
   const id = source.split("/")[2];
 
   return (
     <Button
-      component={Link}
-      to={`/${resource}/${id}/show`}
+      onClick={async () => {
+        // fetch the resource to ensure it still exists before redirecting
+        const { data } = await dataProvider
+          .getOne(resource, { id })
+          .catch(() => ({ data: undefined })); // not found
+
+        if (data) {
+          navigate(`/${resource}/${id}/show`);
+        } else {
+          notify(
+            `The concerned resource (${resource}/${id}) no longer exists.`,
+            { type: "error" },
+          );
+        }
+      }}
       label="Go to resource"
       startIcon={<DataObjectIcon />}
       {...rest}
