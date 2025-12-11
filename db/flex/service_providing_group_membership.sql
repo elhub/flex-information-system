dd4d12329349c71fef12d2b30798d5b9e4dc8008
Kristoffer Lorentsen <kristoffer.lorentsen@statnett.no>
2025-12-11 09:43:18 +0100

    feat: spgm bidding zone validation rule (FLEX-900) (#468)

--- db/flex/service_providing_group_membership.sql
+++ db/flex/service_providing_group_membership.sql
@@ -33,6 +33,7 @@ CREATE TABLE IF NOT EXISTS service_providing_group_membership (
 );
 
 -- changeset flex:service-providing-group-membership-consistency-on-valid-time-increase runOnChange:true endDelimiter:--
+-- SPGM-VAL001: The controllable unit and service providing group must belong to the same service provider.
 CREATE OR REPLACE FUNCTION consistency_on_spgm_valid_time_increase()
 RETURNS trigger
 LANGUAGE plpgsql
@@ -66,10 +67,61 @@ BEGIN
 END;
 $$;
 
--- changeset flex:service-providing-group-membership-upsert-consistency-trigger runOnChange:true endDelimiter:--
-CREATE OR REPLACE TRIGGER service_providing_group_membership_upsert_consistency
+-- changeset flex:service-providing-group-membership-cusp-consistency-trigger runOnChange:true endDelimiter:--
+CREATE OR REPLACE TRIGGER service_providing_group_membership_cusp_consistency
 BEFORE INSERT OR UPDATE ON service_providing_group_membership
 FOR EACH ROW EXECUTE PROCEDURE consistency_on_spgm_valid_time_increase();
+
+-- changeset flex:service-providing-group-membership-upsert-consistency-trigger-drop runOnChange:true endDelimiter:--
+-- TODO: remove once rollout is complete
+DROP TRIGGER IF EXISTS service_providing_group_membership_upsert_consistency
+ON service_providing_group_membership;
+
+-- changeset flex:service-providing-group-membership-bidding-zone-consistency runOnChange:true endDelimiter:--
+-- SPGM-VAL002: The bidding zone of the SPG must be consistent with the bidding zone of the CU.
+CREATE OR REPLACE FUNCTION
+service_providing_group_membership_bidding_zone_consistency()
+RETURNS trigger
+LANGUAGE plpgsql
+SECURITY DEFINER
+AS $$
+DECLARE
+  -- the new SPGM is covered by the CU-BZ valid timeline
+  lv_covered boolean;
+  lv_spg_bidding_zone text;
+BEGIN
+
+    select bidding_zone into lv_spg_bidding_zone
+    from flex.service_providing_group spg
+    where spg.id = NEW.service_providing_group_id;
+
+    select
+        COALESCE(range_agg(cubz.valid_time_range) @> NEW.valid_time_range, false)
+    into lv_covered
+    from flex.controllable_unit_bidding_zone as cubz
+    where cubz.controllable_unit_id = NEW.controllable_unit_id
+    and cubz.bidding_zone = lv_spg_bidding_zone
+    and cubz.valid_time_range && NEW.valid_time_range;
+
+    IF NOT lv_covered THEN
+      RAISE 'The CU is not in the SPGs bidding zone % from % to %',
+        lv_spg_bidding_zone,
+        timeline.timestamptz_to_text(lower(NEW.valid_time_range)),
+        timeline.timestamptz_to_text(upper(NEW.valid_time_range));
+    END IF;
+
+    RETURN NEW;
+END;
+$$;
+
+-- changeset flex:service-providing-group-membership-bidding-zone-consistency-trigger runOnChange:true endDelimiter:--
+CREATE OR REPLACE TRIGGER
+service_providing_group_membership_bidding_zone_consistency
+BEFORE INSERT OR UPDATE ON
+service_providing_group_membership
+FOR EACH ROW EXECUTE PROCEDURE
+service_providing_group_membership_bidding_zone_consistency();
+
 
 -- changeset flex:service-providing-group-insert-grip-prequalificaton-function runOnChange:true endDelimiter:--
 CREATE OR REPLACE FUNCTION
