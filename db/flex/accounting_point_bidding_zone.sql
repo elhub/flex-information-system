fe9567253be68f1e55011cd671de116a5a9d1bc1
Enzo Crance <enzo.crance@statnett.no>
2026-01-14 15:50:28 +0100

    feat: repoint AP-BZ/SO views to MGA-BZ/SO (FLEX-949)

--- db/flex/accounting_point_bidding_zone.sql
+++ db/flex/accounting_point_bidding_zone.sql
@@ -6,11 +6,15 @@ CREATE OR REPLACE VIEW accounting_point_bidding_zone
 WITH (security_invoker = false) AS (
     SELECT
         ap_mga.accounting_point_id,
-        mga.price_area AS bidding_zone,
-        ap_mga.valid_time_range
+        mga_bz.bidding_zone,
+        (
+            ap_mga.valid_time_range
+            * mga_bz.valid_time_range
+        ) AS valid_time_range
     FROM flex.accounting_point_metering_grid_area AS ap_mga
-        INNER JOIN flex.metering_grid_area AS mga
-            ON ap_mga.metering_grid_area_id = mga.id
+        INNER JOIN flex.metering_grid_area_bidding_zone AS mga_bz
+            ON ap_mga.metering_grid_area_id = mga_bz.metering_grid_area_id
+                AND ap_mga.valid_time_range && mga_bz.valid_time_range
 );
 
 -- changeset flex:accounting-point-bidding-zone-grants runAlways:true endDelimiter:;
--- db/flex/accounting_point_system_operator.sql
+++ db/flex/accounting_point_system_operator.sql
@@ -6,13 +6,16 @@ CREATE OR REPLACE VIEW accounting_point_system_operator
 WITH (security_invoker = false) AS (
     SELECT
         ap_mga.accounting_point_id,
-        mga.system_operator_id,
+        mga_so.system_operator_id,
         row_number() OVER () AS id,
-        (ap_mga.valid_time_range * mga.valid_time_range) AS valid_time_range
-    FROM accounting_point_metering_grid_area AS ap_mga
-        INNER JOIN metering_grid_area AS mga
-            ON ap_mga.metering_grid_area_id = mga.id
-                AND ap_mga.valid_time_range && mga.valid_time_range
+        (
+            ap_mga.valid_time_range
+            * mga_so.valid_time_range
+        ) AS valid_time_range
+    FROM flex.accounting_point_metering_grid_area AS ap_mga
+        INNER JOIN flex.metering_grid_area_system_operator AS mga_so
+            ON ap_mga.metering_grid_area_id = mga_so.metering_grid_area_id
+                AND ap_mga.valid_time_range && mga_so.valid_time_range
 );
 
 -- changeset flex:accounting-point-system-operator-grants runAlways:true endDelimiter:;
