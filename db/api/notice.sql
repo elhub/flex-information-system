589cb81ae6b888ae3f3311af6dbdc3876745835a
Enzo Crance <enzo.crance@statnett.no>
2026-02-04 13:23:07 +0100

    feat(db): redirect notice API view to notice table (FLEX-966)

--- db/api/notice.sql
+++ db/api/notice.sql
@@ -3,21 +3,19 @@
 
 -- changeset flex:api-notice-create endDelimiter:-- runAlways:true
 CREATE OR REPLACE VIEW api.notice
-WITH (security_invoker = false, security_barrier = true) AS ( -- cf AP-BRP
+WITH (security_invoker = true) AS (
     SELECT
+        id,
         data,
         party_id,
         type,
+        status,
+        recorded_by,
         CASE
             WHEN source_resource IS null OR source_id IS null THEN null
             ELSE '/' || source_resource || '/' || source_id
         END
-        AS source
-    FROM flex.notice_fresh
-    -- no RLS policies for views, so we need to have them built-in
-    WHERE
-        -- RLS: NOTICE-FISO001
-        current_role = 'flex_flexibility_information_system_operator'
-        -- RLS: NOTICE-COM001
-        OR party_id = (SELECT flex.current_party())
+        AS source,
+        lower(record_time_range) AS recorded_at
+    FROM flex.notice
 );
