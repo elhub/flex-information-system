2733cce6b691c77be7be0ffbc25f19abc45ca719
Enzo Crance <enzo.crance@statnett.no>
2026-02-09 16:53:54 +0100

    chore: restore party_staging to main (FLEX-1025)

--- db/flex/party_staging.sql
+++ db/flex/party_staging.sql
@@ -3,15 +3,14 @@
 
 -- landing table for party data updates coming from external sources
 -- NOTE: when testing locally, run test_data.fill_party_staging() to populate
--- changeset flex:party-staging-create runOnChange:true endDelimiter:--
+-- changeset flex:party-staging-create runOnChange:false endDelimiter:--
+--validCheckSum: 9:60c2aad8008fbfdb41a4f7be8cfff49f
 CREATE TABLE IF NOT EXISTS party_staging (
     id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     gln text CHECK (validate_business_id(gln, 'gln')),
     org text NOT NULL CHECK (validate_business_id(org, 'org')),
     name text NOT NULL,
-    type text NOT NULL,
-
-    CONSTRAINT party_staging_type_check CHECK (
+    type text NOT NULL CHECK (
         type IN (
             'balance_responsible_party',
             'end_user',
@@ -24,5 +23,6 @@ CREATE TABLE IF NOT EXISTS party_staging (
             'third_party'
         )
     ),
+
     CONSTRAINT uk_party_staging_gln_type UNIQUE (gln, type)
 );
