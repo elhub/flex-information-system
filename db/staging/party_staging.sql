92877ba52b26f72aeb2e935a717902b3aa12fde7
Enzo Crance <enzo.crance@statnett.no>
2026-02-12 16:05:24 +0100

    feat(db): party_staging view in staging schema (FLEX-1043)

---
+++ db/staging/party_staging.sql
@@ -0,0 +1,12 @@
+--liquibase formatted sql
+-- Manually managed file
+
+-- changeset flex:party-staging-view runOnChange:true endDelimiter:--
+CREATE OR REPLACE VIEW party_staging_v AS (
+    SELECT
+        gln,
+        org,
+        name,
+        type
+    FROM flex.party_staging
+);
