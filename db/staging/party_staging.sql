6ec5fa5a9b802b33757a4d8cb700ffec170a9162
Enzo Crance <enzo.crance@statnett.no>
2026-02-16 09:48:57 +0100

    docs: explain view purpose (FLEX-1043)

--- db/staging/party_staging.sql
+++ db/staging/party_staging.sql
@@ -2,6 +2,7 @@
 -- Manually managed file
 
 -- changeset flex:party-staging-view runOnChange:true endDelimiter:--
+-- abstraction/decoupling layer above the party staging table
 CREATE OR REPLACE VIEW staging.party_staging_v AS (
     SELECT
         gln,
