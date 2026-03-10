de8f10ec8bb3f72f1afcd7c9805b4f8bace0b25c
elhub-no <80970159+elhub-no@users.noreply.github.com>
2026-03-10 10:10:37 +0100

    chore(deps): update local (#660)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>
    Co-authored-by: Enzo Crance <enzo.crance@statnett.no>

--- test/flex/api/service_providing_group_grid_prequalification/list_service_providing_group_grid_prequalification_history.py
+++ test/flex/api/service_providing_group_grid_prequalification/list_service_providing_group_grid_prequalification_history.py
@@ -24,6 +24,7 @@ def _get_kwargs(
     limit: str | Unset = UNSET,
     service_providing_group_grid_prequalification_id: str | Unset = UNSET,
 ) -> dict[str, Any]:
+
     params: dict[str, Any] = {}
 
     params["id"] = id
