eb1034d1e73c132c1b1e36e8eeb663a5ed628f75
Kristoffer Lorentsen <kristoffer.lorentsen@statnett.no>
2026-03-18 20:00:15 +0100

    chore: why-comment for enumValue slicing (FLEX-1038)

--- frontend/src/components/EDS-ra/inputs/EnumArrayInput.tsx
+++ frontend/src/components/EDS-ra/inputs/EnumArrayInput.tsx
@@ -14,6 +14,8 @@ export const EnumArrayInput = ({ enumKey, ...rest }: EnumArrayInputProps) => {
   const i18nProvider = useI18nProvider() as I18nProvider;
 
   const options = i18nProvider.getEnumValues(enumKey).map((value) => ({
+    // enum values are in the format "enumKey.enumValue",
+    // so we need to remove the enumKey part (plus one dot) to get the actual value
     value: value.slice(enumKey.length + 1),
     label: translate(`enum.${value}`),
   }));
