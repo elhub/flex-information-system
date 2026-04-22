812faf0d0b0353b93c42d490ed94c887c3b06c8e
Kenneth Strand <kenneth.strand@statnett.no>
2026-04-22 16:02:18 +0200

    fix: Use named check constraints + fix translation (FLEX-1136)

--- db/flex/accounting_point_grid_location.sql
+++ db/flex/accounting_point_grid_location.sql
@@ -5,24 +5,39 @@
 CREATE TABLE IF NOT EXISTS accounting_point_grid_location (
     id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     accounting_point_id bigint NOT NULL,
-    object_type text NOT NULL CHECK (
-        object_type IN ('substation', 'transformer')
-    ),
+    object_type text NOT NULL,
     business_id text,
-    name text NOT NULL CHECK ((char_length(name) <= 512)),
-    nominal_voltage numeric NOT NULL CHECK (nominal_voltage >= 0),
+    name text NOT NULL,
+    nominal_voltage decimal(9, 3) NOT NULL,
     additional_information text,
-    source text NOT NULL CHECK (
-        source IN ('cso', 'so', 'grid_model', 'system')
-    ),
-    quality text NOT NULL CHECK (
-        quality IN ('confirmed', 'guessed')
-    ),
+    source text NOT NULL,
+    quality text NOT NULL,
     -- audit fields
     record_time_range tstzrange NOT NULL DEFAULT tstzrange(
         localtimestamp, NULL, '[)'
     ),
     recorded_by bigint NOT NULL DEFAULT current_identity(),
+
+    CONSTRAINT accounting_point_grid_location_object_type_check CHECK (
+        object_type IN ('substation', 'transformer')
+    ),
+    CONSTRAINT accounting_point_grid_location_business_id_check CHECK (
+        business_id IS NULL
+        OR business_id
+        ~ '^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$'
+    ),
+    CONSTRAINT accounting_point_grid_location_name_check CHECK (
+        char_length(name) <= 512
+    ),
+    CONSTRAINT accounting_point_grid_location_nominal_voltage_check CHECK (
+        nominal_voltage >= 0
+    ),
+    CONSTRAINT accounting_point_grid_location_source_check CHECK (
+        source IN ('cso', 'so', 'grid_model', 'system')
+    ),
+    CONSTRAINT accounting_point_grid_location_quality_check CHECK (
+        quality IN ('confirmed', 'guessed')
+    ),
 
     CONSTRAINT fk_accounting_point_grid_location_accounting_point FOREIGN KEY (
         accounting_point_id
@@ -35,6 +50,6 @@ BEFORE UPDATE ON accounting_point_grid_location
 FOR EACH ROW EXECUTE FUNCTION suppress_redundant_updates_trigger();
 
 -- changeset flex:accounting-point-grid-location-accounting-point-idx runOnChange:true endDelimiter:-- runInTransaction:false
-CREATE INDEX CONCURRENTLY IF NOT EXISTS
+CREATE UNIQUE INDEX CONCURRENTLY IF NOT EXISTS
 accounting_point_grid_location_accounting_point_idx
 ON accounting_point_grid_location (accounting_point_id);
