bf70c9470231197f1791a75a36cc39249cc33dd0
August Solvang <august.solvang@statnett.no>
2026-05-08 10:19:05 +0200

    feat: add useControllableUnits dashboard hook

---
+++ frontend/src/dashboard/hooks/useControllableUnits.ts
@@ -0,0 +1,21 @@
+import { useGetIdentity } from "ra-core";
+import { useQuery } from "@tanstack/react-query";
+import { listControllableUnitServiceProvider } from "../../generated-client";
+import { throwOnError } from "../../util";
+
+export const useControllableUnits = () => {
+  const { data: identity } = useGetIdentity();
+  const partyID = identity?.partyID as number | undefined;
+
+  return useQuery({
+    queryKey: ["dashboard-controllable-units", { service_provider_id: partyID }],
+    enabled: partyID != null,
+    queryFn: () =>
+      listControllableUnitServiceProvider({
+        query: {
+          service_provider_id: `eq.${partyID}`,
+          embed: "controllable_unit",
+        },
+      }).then(throwOnError),
+  });
+};
