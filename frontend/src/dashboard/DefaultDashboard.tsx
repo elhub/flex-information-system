f901f2ca2012a4949335f6623a8bd8e1c22c01da
August Solvang <august.solvang@statnett.no>
2026-05-13 13:35:40 +0200

    fix: remove duplicate heading from DefaultDashboard (FLEX-1191)

--- frontend/src/dashboard/DefaultDashboard.tsx
+++ frontend/src/dashboard/DefaultDashboard.tsx
@@ -1,9 +1,3 @@
-import { Heading } from "../components/ui";
-
 export const DefaultDashboard = () => {
-  return (
-    <Heading level={2} size="large">
-      Dashboard
-    </Heading>
-  );
+  return null;
 };
