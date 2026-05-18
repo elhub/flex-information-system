754837e8312df50d059f7e5c0804e7d5334cd48e
Kenneth Strand <kenneth.strand@statnett.no>
2026-05-18 11:41:23 +0200

    feat: Persist active tab in URL search param for all show pages (FLEX-1205) (#843)

--- frontend/src/controllable_unit/show/ControllableUnitShowTabs.tsx
+++ frontend/src/controllable_unit/show/ControllableUnitShowTabs.tsx
@@ -1,20 +1,24 @@
 import { Tabs } from "../../components/ui";
 import { TechnicalResourceList } from "../technical_resource/TechnicalResourceList";
 import { RecordContextProvider } from "ra-core";
+import { useTabSearchParam } from "../../hooks/useTabSearchParam";
 
 type Props = {
   cuId: number;
 };
 
-export const ControllableUnitShowTabs = ({ cuId }: Props) => (
-  <Tabs defaultValue="technical_resources" className="relative top-[-24px]">
-    <Tabs.List>
-      <Tabs.Tab label="Technical resources" value="technical_resources" />
-    </Tabs.List>
-    <Tabs.Panel value="technical_resources">
-      <RecordContextProvider value={{ id: cuId }}>
-        <TechnicalResourceList />
-      </RecordContextProvider>
-    </Tabs.Panel>
-  </Tabs>
-);
+export const ControllableUnitShowTabs = ({ cuId }: Props) => {
+  const [tab, setTab] = useTabSearchParam("technical_resources");
+  return (
+    <Tabs value={tab} onChange={setTab} className="relative top-[-24px]">
+      <Tabs.List>
+        <Tabs.Tab label="Technical resources" value="technical_resources" />
+      </Tabs.List>
+      <Tabs.Panel value="technical_resources">
+        <RecordContextProvider value={{ id: cuId }}>
+          <TechnicalResourceList />
+        </RecordContextProvider>
+      </Tabs.Panel>
+    </Tabs>
+  );
+};
--- frontend/src/service_providing_group/show/ServiceProvidingGroupShowTabs.tsx
+++ frontend/src/service_providing_group/show/ServiceProvidingGroupShowTabs.tsx
@@ -5,6 +5,7 @@ import { ServiceProvidingGroupShowGridPrequalificationsTable } from "./ServicePr
 import { ServiceProvidingGroupShowSPGSummarySection } from "./ServiceProvidingGroupShowSPGSummarySection";
 import { ServiceProvidingGroupSummary } from "../../generated-client";
 import { SpgShowViewModel } from "./useSpgShowViewModel";
+import { useTabSearchParam } from "../../hooks/useTabSearchParam";
 
 type Props = {
   spgId: number;
@@ -16,32 +17,38 @@ export const ServiceProvidingGroupShowTabs = ({
   spgId,
   spgViewModel,
   summary,
-}: Props) => (
-  <Tabs defaultValue="summary" className="relative top-[-24px]">
-    <Tabs.List>
-      <Tabs.Tab label="Summary" value="summary" />
-      <Tabs.Tab label="Controllable units" value="controllable_units" />
-      <Tabs.Tab label="Product applications" value="product_applications" />
-      <Tabs.Tab label="Grid prequalifications" value="grid_prequalifications" />
-    </Tabs.List>
-    <Tabs.Panel value="summary">
-      {summary ? (
-        <ServiceProvidingGroupShowSPGSummarySection
-          spgViewModel={spgViewModel}
-          summary={summary}
+}: Props) => {
+  const [tab, setTab] = useTabSearchParam("summary");
+  return (
+    <Tabs value={tab} onChange={setTab} className="relative top-[-24px]">
+      <Tabs.List>
+        <Tabs.Tab label="Summary" value="summary" />
+        <Tabs.Tab label="Controllable units" value="controllable_units" />
+        <Tabs.Tab label="Product applications" value="product_applications" />
+        <Tabs.Tab
+          label="Grid prequalifications"
+          value="grid_prequalifications"
         />
-      ) : (
-        "No summary available"
-      )}
-    </Tabs.Panel>
-    <Tabs.Panel value="controllable_units">
-      <ServiceProvidingGroupShowTable spgId={spgId} />
-    </Tabs.Panel>
-    <Tabs.Panel value="product_applications">
-      <ServiceProvidingGroupShowProductApplicationsTable spgId={spgId} />
-    </Tabs.Panel>
-    <Tabs.Panel value="grid_prequalifications">
-      <ServiceProvidingGroupShowGridPrequalificationsTable spgId={spgId} />
-    </Tabs.Panel>
-  </Tabs>
-);
+      </Tabs.List>
+      <Tabs.Panel value="summary">
+        {summary ? (
+          <ServiceProvidingGroupShowSPGSummarySection
+            spgViewModel={spgViewModel}
+            summary={summary}
+          />
+        ) : (
+          "No summary available"
+        )}
+      </Tabs.Panel>
+      <Tabs.Panel value="controllable_units">
+        <ServiceProvidingGroupShowTable spgId={spgId} />
+      </Tabs.Panel>
+      <Tabs.Panel value="product_applications">
+        <ServiceProvidingGroupShowProductApplicationsTable spgId={spgId} />
+      </Tabs.Panel>
+      <Tabs.Panel value="grid_prequalifications">
+        <ServiceProvidingGroupShowGridPrequalificationsTable spgId={spgId} />
+      </Tabs.Panel>
+    </Tabs>
+  );
+};
