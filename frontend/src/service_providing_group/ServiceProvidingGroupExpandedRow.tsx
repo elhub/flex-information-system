32e87f7f94ddadb8ed547b9dda673dc24a48e8d4
Kenneth Strand <kenneth.strand@statnett.no>
2026-05-26 14:52:25 +0200

    fix: prettier (FLEX-1211)

--- frontend/src/service_providing_group/ServiceProvidingGroupExpandedRow.tsx
+++ frontend/src/service_providing_group/ServiceProvidingGroupExpandedRow.tsx
@@ -113,7 +113,11 @@ export const ServiceProvidingGroupExpandedRow = ({ record }: Props) => {
         {isLoadingPrequal ? (
           <Loader />
         ) : !prequalifications || prequalifications.length === 0 ? (
-          <EmptyBox message={translate("text.spg_grid_prequalification_empty", { _: "No grid prequalifications" })} />
+          <EmptyBox
+            message={translate("text.spg_grid_prequalification_empty", {
+              _: "No grid prequalifications",
+            })}
+          />
         ) : (
           prequalifications.map((prequal) => {
             const variant =
@@ -156,7 +160,11 @@ export const ServiceProvidingGroupExpandedRow = ({ record }: Props) => {
         {isLoadingPa ? (
           <Loader />
         ) : !productApplications || productApplications.length === 0 ? (
-          <EmptyBox message={translate("text.spg_product_application_empty", { _: "No product applications" })} />
+          <EmptyBox
+            message={translate("text.spg_product_application_empty", {
+              _: "No product applications",
+            })}
+          />
         ) : (
           productApplications.map((pa) => {
             const variant =
