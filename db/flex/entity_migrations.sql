ade402189094e3486fce64431426fd8db1f5f2ae
Enzo Crance <enzo.crance@statnett.no>
2026-06-10 10:54:50 +0200

    chore: remove migration (FLEX-1280)

--- db/flex/entity_migrations.sql
+++ db/flex/entity_migrations.sql
@@ -9,10 +9,3 @@ ADD CONSTRAINT check_entity_type_business_id_type CHECK (
     (type = 'person' AND business_id_type IN ('pid', 'email'))
     OR (type = 'organisation' AND business_id_type = 'org')
 );
-
--- changeset flex:entity-business-id-email-lowercase runOnChange:false endDelimiter:;
---preconditions onFail:MARK_RAN
---precondition-sql-check expectedResult:t SELECT EXISTS (SELECT 1 FROM flex.entity WHERE business_id_type = 'email' AND business_id != lower(business_id))
-UPDATE flex.entity
-SET business_id = lower(business_id)
-WHERE business_id_type = 'email' AND business_id != lower(business_id);
