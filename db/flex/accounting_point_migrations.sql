7767046c73f392a056b4af0a5ca3165e5839514d
Enzo Crance <enzo.crance@statnett.no>
2026-06-11 13:44:26 +0200

    refactor: split changesets (FLEX-1133)

--- db/flex/accounting_point_migrations.sql
+++ db/flex/accounting_point_migrations.sql
@@ -11,5 +11,8 @@ ADD COLUMN IF NOT EXISTS location GEOMETRY (POINT, 4326) NULL;
 ALTER TABLE flex.accounting_point
 ADD COLUMN IF NOT EXISTS location GEOMETRY (POINT, 4326) NULL;
 
+-- changeset flex:accounting-point-location-index runOnChange:false endDelimiter:;
+--preconditions onFail:MARK_RAN
+--precondition-sql-check expectedResult:t SELECT EXISTS (SELECT 1 FROM information_schema.columns WHERE table_schema = 'flex' AND table_name = 'accounting_point' AND column_name = 'location');
 CREATE INDEX IF NOT EXISTS accounting_point_location_idx
 ON flex.accounting_point USING gist (location);
