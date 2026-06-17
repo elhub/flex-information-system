7b2f8157a97acc8a6d431f943c052ea2da2b6736
Kenneth Strand <kenneth.strand@statnett.no>
2026-06-17 15:28:39 +0200

    chore(kbackend): do not log error on cancellation exception, e.g. sigterm (FLEX-1267)

--- kbackend/src/main/kotlin/no/elhub/flex/config/Scheduling.kt
+++ kbackend/src/main/kotlin/no/elhub/flex/config/Scheduling.kt
@@ -2,6 +2,7 @@ package no.elhub.flex.config
 
 import io.github.oshai.kotlinlogging.KotlinLogging
 import io.ktor.server.application.Application
+import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.launch
 import no.elhub.flex.scheduled.AccountingPointSyncScheduler
 import org.koin.ktor.ext.get
@@ -12,6 +13,8 @@ fun Application.configureScheduling() {
     launch {
         try {
             get<AccountingPointSyncScheduler>().start()
+        } catch (ce: CancellationException) {
+            throw ce
         } catch (e: Throwable) {
             logger.error(e) { "Accounting point sync scheduler stopped unexpectedly" }
         }
