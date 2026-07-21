8e70346923a032bb027bfa70feeb296e443779f0
Enzo Crance <enzo.crance@statnett.no>
2026-07-21 10:46:08 +0200

    fix: remove old crashing migration (FLEX-1323)

--- db/flex/entity_client_migrations.sql
+++ db/flex/entity_client_migrations.sql
@@ -1,44 +1,6 @@
 --liquibase formatted sql
 -- Manually managed file
 
--- changeset flex:entity-client-party-scopes runOnChange:false endDelimiter:;
---preconditions onFail:MARK_RAN
---precondition-sql-check expectedResult:0 SELECT COUNT(*) FROM information_schema.columns WHERE table_schema = 'flex' AND table_name = 'entity_client' AND column_name = 'scopes'
-ALTER TABLE flex.entity_client
-DISABLE TRIGGER USER;
-
--- add scopes
-ALTER TABLE flex.entity_client
-ADD COLUMN scopes flex.scope [];
-
-UPDATE flex.entity_client
-SET scopes = '{manage:data, manage:auth}'
-WHERE scopes IS null OR array_length(scopes, 1) = 0;
-
-ALTER TABLE flex.entity_client
-ALTER COLUMN scopes SET NOT NULL;
-
-ALTER TABLE flex.entity_client
-ADD CONSTRAINT check_scopes_not_empty CHECK (
-    array_length(scopes, 1) > 0
-);
-
-ALTER TABLE flex.entity_client_history
-ADD COLUMN scopes flex.scope [];
-
-UPDATE flex.entity_client_history
-SET scopes = '{manage:data, manage:auth}';
-
--- add party
-ALTER TABLE flex.entity_client
-ADD COLUMN party_id bigint;
-
-ALTER TABLE flex.entity_client_history
-ADD COLUMN party_id bigint;
-
-ALTER TABLE flex.entity_client
-ENABLE TRIGGER USER;
-
 -- changeset flex:entity-client-attachment-scope runOnChange:false endDelimiter:;
 ALTER TABLE flex.entity_client
 DISABLE TRIGGER USER;
@@ -86,6 +48,7 @@ ALTER TABLE flex.entity_client
 ENABLE TRIGGER USER;
 
 -- changeset flex:entity-client-check-assumable-party-function runOnChange:true endDelimiter:--
+-- trigger to restrict the party field (can only be added once the field is there)
 CREATE OR REPLACE FUNCTION entity_client_check_assumable_party()
 RETURNS trigger
 SECURITY DEFINER
