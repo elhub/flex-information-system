982b4594b654f4670eeaa2079f19f1fb9336b267
Simen <simen.jakobsen@statnett.no>
2026-08-24 10:51:12 +0200

    fix: only perform one call to backend from frontend (FLEX-1427)

--- frontend/src/accounting_point/AccountingPointLinkField.tsx
+++ frontend/src/accounting_point/AccountingPointLinkField.tsx
@@ -1,4 +1,4 @@
-import { useRecordContext, useGetManyAggregate } from "ra-core";
+import { useGetManyAggregate, useRecordContext } from "ra-core";
 import { Link as RouterLink } from "react-router-dom";
 import { BodyText, Link } from "../components/ui";
 
@@ -13,20 +13,23 @@ export const AccountingPointLinkField = ({
   const record = useRecordContext();
   const accountingPointId = record?.[_source];
 
+  let accountingPoint = record?.accounting_point;
   const { data } = useGetManyAggregate(
     "accounting_point",
     { ids: accountingPointId ? [accountingPointId] : [] },
-    { enabled: !!accountingPointId },
+    { enabled: !!accountingPoint },
   );
 
-  const ap = data?.[0];
+  if (data) {
+    accountingPoint = data?.[0];
+  }
 
-  if (!ap) return <BodyText size="small">-</BodyText>;
+  if (!accountingPoint) return <BodyText size="small">-</BodyText>;
 
   return (
     <span onClick={(e) => e.stopPropagation()}>
-      <Link as={RouterLink} to={`/accounting_point/${ap.id}/show`}>
-        {ap.business_id}
+      <Link as={RouterLink} to={`/accounting_point/${accountingPoint.id}/show`}>
+        {accountingPoint.business_id}
       </Link>
     </span>
   );
