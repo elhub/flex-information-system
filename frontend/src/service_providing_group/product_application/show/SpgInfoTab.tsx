5cc4b4ec0135cc351e68e6cb60d1787882ea9faa
Trine Cecilia Peinert <cecilia.peinert@statnett.no>
2026-08-24 15:53:54 +0200

    feat(ui): remove spg status from spgpa and grid prequal (FLEX-1422) (#1118)

--- frontend/src/service_providing_group/product_application/show/SpgInfoTab.tsx
+++ frontend/src/service_providing_group/product_application/show/SpgInfoTab.tsx
@@ -1,7 +1,5 @@
-import { Loader, Badge, Link, Panel } from "../../../components/ui";
+import { Loader, Link, Panel } from "../../../components/ui";
 import { LabelValue } from "../../../components/LabelValue";
-import { useTranslateEnum } from "../../../intl/intl";
-import { spgStatusVariantMap } from "../../serviceProvidingGroupStatus";
 import { Link as RouterLink } from "react-router-dom";
 import { ServiceProvidingGroup } from "../../../generated-client";
 import { ServiceProvidingGroupControllableUnitSummary } from "../../summary/ServiceProvidingGroupControllableUnitSummary";
@@ -19,13 +17,9 @@ export const SpgInfoTab = ({
   spg,
   powerScale = KILO,
 }: Props) => {
-  const translateEnum = useTranslateEnum();
-
   if (!spg) {
     return <Loader size="small" />;
   }
-
-  const { status, icon } = spgStatusVariantMap[spg.status];
 
   return (
     <div className="flex flex-col gap-4">
@@ -45,14 +39,6 @@ export const SpgInfoTab = ({
           }
         />
         <LabelValue label="Bidding zone" value={spg.bidding_zone} />
-        <LabelValue
-          label="Status"
-          value={
-            <Badge size="small" status={status} variant="block" icon={icon}>
-              {translateEnum(`service_providing_group.status.${spg.status}`)}
-            </Badge>
-          }
-        />
       </Panel>
       {spg.summary && (
         <>
