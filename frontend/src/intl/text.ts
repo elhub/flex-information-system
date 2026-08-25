b7fe62a913a761f2cd197c3dac7269ab9eaf8f0c
Cecilia Peinert <cecilia.peinert@statnett.no>
2026-08-25 12:02:38 +0200

    fix(ui): update text spg percentage tooltip (FLEX-1429)

--- frontend/src/intl/text.ts
+++ frontend/src/intl/text.ts
@@ -15,6 +15,7 @@ export type TextKey =
   | "controllable_unit.is_small.false.label"
   | "cu_flexible_power_exceeds_rated_power_heading"
   | "cu_flexible_power_exceeds_rated_power_body"
+  | "power_ratio_tooltip"
   | "lookup.input.accounting_point"
   | "lookup.input.controllable_unit"
   | "lookup.input.end_user"
@@ -119,6 +120,8 @@ export const text: Record<string, Record<TextKey, string>> = {
       "Flexible power exceeds rated power",
     cu_flexible_power_exceeds_rated_power_body:
       "The flexible power of this controllable unit exceeds the combined maximum active power of all its technical resources. Update the flexible power or add technical resources.",
+    power_ratio_tooltip:
+      "The flexible power represents %{percentage}% of the rated power",
     "lookup.input.accounting_point": "Accounting point",
     "lookup.input.controllable_unit": "Controllable unit",
     "lookup.input.end_user": "End user",
@@ -249,6 +252,8 @@ export const text: Record<string, Record<TextKey, string>> = {
       "Fleksibel effekt overstiger installert effekt",
     cu_flexible_power_exceeds_rated_power_body:
       "Den fleksible effekten til denne kontrollerbare enheten overstiger merkeeffekten. Oppdater fleksibel effekt eller legg til tekniske ressurser.",
+    power_ratio_tooltip:
+      "Den fleksible effekten utgjør %{percentage}% av merkeeffekten",
     "lookup.input.accounting_point": "Avregningspunkt",
     "lookup.input.controllable_unit": "Kontrollerbar enhet",
     "lookup.input.end_user": "Sluttbruker",
