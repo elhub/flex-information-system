23f24208b0b9b15084d28e4fdd43aeaaef97a0f5
Simen Jakobsen <72787997+simenja@users.noreply.github.com>
2026-08-25 10:02:12 +0200

    fix: remove row_number as id to see if it improves performance (FLEX-1427) (#1122)
    Co-authored-by: Kristoffer Lorentsen <kristoffer.lorentsen@statnett.no>

--- db/flex/accounting_point_system_operator.sql
+++ db/flex/accounting_point_system_operator.sql
@@ -7,7 +7,10 @@ WITH (security_invoker = false) AS (
     SELECT
         ap_mga.accounting_point_id,
         mga_so.system_operator_id,
-        row_number() OVER () AS id,
+        -- hardcoded id since it is not really in use
+        -- we had a previous (costly) implementation using row_number
+        -- cannot change the view definition w/o doing lots of refactoring
+        1 AS id,
         (
             ap_mga.valid_time_range
             * mga_so.valid_time_range
