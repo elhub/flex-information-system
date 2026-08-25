41faf34f0a2d0df786875c722ee6c95b5ee4eebc
Simen <simen.jakobsen@statnett.no>
2026-08-25 12:07:13 +0200

    fix: encoding in translation (FLEX-1395)

--- frontend/src/intl/text.ts
+++ frontend/src/intl/text.ts
@@ -354,7 +354,7 @@ export const text: Record<string, Record<TextKey, string>> = {
       "Kun synlig for din nåværende aktør",
     "comment.visibility.any_involved_party.description":
       "Synlig for alle parter involvert i denne ressursen",
-    notice_spg_membership_button: "G� til medlemskap for fleksibilitetsgruppe",
+    notice_spg_membership_button: "Gå til medlemskap for fleksibilitetsgruppe",
     notice_missing_grid_location_button:
       "Sjekk informasjon om nettlokasjon her",
     notice_insufficient_grid_location_source_button: "Gå til målepunktet",
