8b7fafca76eb2b62f352ab37a7520dbee7d5e5a7
Simen <simen.jakobsen@statnett.no>
2026-09-25 10:13:17 +0200

    fix: (FLEX-1471) Revert "Replace label with labelKey for metering grid area"
    This reverts commit c3e0096044524efbde8fb09b7bb9f7ac550b03d8.

--- frontend/src/controllable_unit/show/ControllableUnitShowSummary.tsx
+++ frontend/src/controllable_unit/show/ControllableUnitShowSummary.tsx
@@ -53,7 +53,8 @@ export const useControllableUnitShowSummary = ({
         : undefined,
     },
     {
-      labelKey: "accounting_point_metering_grid_area.metering_grid_area_id",
+      label: "Metering grid area",
+      value: meteringGridArea?.name,
     },
     {
       label: "Bidding zone",
